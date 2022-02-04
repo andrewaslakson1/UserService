0808906ce18f78e7fcc23034ab18824c437cedbf
Andrew Aslakson <andrew01177@gmail.com>
2022-02-04 12:29:58 -0500

    added Main driver class, also added lombok to pom and created pom variables

---
+++ src/main/java/my/project/Driver.java
@@ -0,0 +1,11 @@
+package my.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Driver {
+    public void main(String[] args) {
+        SpringApplication.run(Driver.class, args);
+    }
+}
