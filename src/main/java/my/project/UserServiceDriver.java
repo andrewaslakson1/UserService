fef8df861a6ca3e7b721d3783daaf68ad7c9468d
Andrew Aslakson <andrew01177@gmail.com>
2022-02-07 18:39:17 -0500

    added configuration to application-local.yml for h2 database console

--- src/main/java/my/project/UserServiceDriver.java
+++ src/main/java/my/project/UserServiceDriver.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceDriver {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(UserServiceDriver.class, args);
     }
 }
