2cd9f3529058eb6f5a012d53baeba2532677019f
Andrew Aslakson <andrew01177@gmail.com>
2022-02-10 12:07:57 -0500

    added bean to driver for CollectorRegistry, could not get tests to autogenerate this class

--- src/main/java/my/project/UserServiceDriver.java
+++ src/main/java/my/project/UserServiceDriver.java
@@ -1,11 +1,22 @@
 package my.project;
 
+import io.prometheus.client.CollectorRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusMetricsExportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceDriver {
+
+    private static PrometheusMetricsExportAutoConfiguration metricsConfig = new PrometheusMetricsExportAutoConfiguration();
+
     public static void main(String[] args) {
         SpringApplication.run(UserServiceDriver.class, args);
+    }
+
+    @Bean
+    public static CollectorRegistry getRegistryBean() {
+        return metricsConfig.collectorRegistry();
     }
 }
